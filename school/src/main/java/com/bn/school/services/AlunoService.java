e42697b30c6295fbad6829c992e53194286f9753
unknown <nascimentob.eliel@gmail.com>
2026-02-24 20:36:02 -0300

    projeto criado

---
+++ school/src/main/java/com/bn/school/services/AlunoService.java
@@ -0,0 +1,26 @@
+package com.bn.school.services;
+
+import com.bn.school.models.AlunoModel;
+import com.bn.school.repositories.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    public List<AlunoModel> findAll(){
+        return  alunoRepository.findAll();
+    }
+
+    public AlunoModel criar (AlunoModel alunoModel){
+        return  alunoRepository.save(alunoModel);
+    }
+
+
+
+}
