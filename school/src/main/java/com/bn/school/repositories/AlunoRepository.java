15594716cbbc37ced52fbaeaa9e3008e4d4c1c29
unknown <nascimentob.eliel@gmail.com>
2026-02-24 21:24:21 -0300

    projeto criado

--- school/src/main/java/com/bn/school/repositories/AlunoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bn.school.repositories;
-
-import com.bn.school.models.AlunoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoRepository extends JpaRepository<AlunoModel,Long> {
-}
